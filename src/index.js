e23dec7d2fdc2f864b52c35e5efb1a3d556be91e
Ibrahim San <ibrahimattiasan@gmail.com>
2023-08-22 11:35:54 +0400

    navbar complete

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-const smallScreenMenu = document.querySelector("#sm-menu");
-const menuOpenBtn = document.querySelector("#open-button-container");
-const menuCloseBtn = document.querySelector("#close-button-container");
+
+const navBar = document.querySelector("#nav");
+const menu = document.querySelector("#menu"); // The container that contains the overlay and menu 
+const menuOpenBtn = document.querySelector("#menu-open"); 
+const menuCloseBtn = document.querySelector("#menu-close"); 
+
 
 menuOpenBtn.addEventListener("click", () => { 
-    smallScreenMenu.classList.toggle("sm-menu-appear");
-    menuOpenBtn.classList.toggle("sm-menu-btn-hide");
-    menuCloseBtn.classList.toggle("sm-menu-btn-appear");
+    menu.classList.toggle("menu-appear");
 })
 
 menuCloseBtn.addEventListener("click", () => { 
-    smallScreenMenu.classList.toggle("sm-menu-appear");
-    menuOpenBtn.classList.toggle("sm-menu-btn-hide");
-    menuCloseBtn.classList.toggle("sm-menu-btn-appear");
+    menu.classList.toggle("menu-appear");
 })
+
+/* To make sure the navbar stays when you scroll and disappear when you scroll down */
+
+let prevScrollpos = window.scrollY; 
+window.onscroll = () => { 
+    let currentScrollpos = window.scrollY; 
+    if (prevScrollpos > currentScrollpos ) {
+        navBar.style.transition = "0.3s ease-in-out"; 
+        navBar.style.top = "0"
+        
+    } else { 
+        navBar.style.transition = "0.3s ease-in-out";
+        navBar.style.top = "-80px"
+        
+    }
+    prevScrollpos = currentScrollpos;
+}
